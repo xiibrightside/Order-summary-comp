0d538ddfb0b98541e0f4b25e1ff80ecf45645f06
xoxo <thakursatyam48@gmail.com>
2022-05-20 19:21:46 +0530

    fixed some errors

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           src={illustration}
           alt="hero illustration"
         ></img>
-        <h2>Order Summary</h2>
+        <h1>Order Summary</h1>
         <p>
           You can now listen to millions of songs, audiobooks, and podcasts on
           any device anywhere you like!
